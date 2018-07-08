48d43b90b45bd4c5f9d1191acfc8f4a191bdaac2
mishkapp <mishkapp@gmail.com>
2018-07-08 13:00:03 +0700

    Added misc functions to operate with players

--- src/sponge_clj/player.clj
+++ src/sponge_clj/player.clj
@@ -1,6 +1,8 @@
 (ns sponge-clj.player
   (:import (org.spongepowered.api.entity.living.player Player)
-           (org.spongepowered.api.item.inventory ItemStack)))
+           (org.spongepowered.api.item.inventory ItemStack)
+           (java.util UUID)
+           (org.spongepowered.api Sponge)))
 
 (defn give-item
   [^Player player ^ItemStack is]
@@ -14,3 +16,28 @@
   [^Player player perm]
   (-> player
       (.hasPermission perm)))
+
+(defn is-player?
+  [obj]
+  (isa? Player obj))
+
+(defn player-name
+  [^Player player]
+  (-> player
+      (.getName)))
+
+(defn player-uuid
+  [^Player player]
+  (-> player
+      (.getUniqueId)))
+
+(defn get-player-by-name
+  [name]
+  (-> (Sponge/getServer)
+      (.getPlayer ^String name)))
+
+(defn get-player-by-uuid
+  [uuid]
+  (let [uuid (if (isa? UUID uuid) uuid (UUID/fromString (str uuid)))]
+    (-> (Sponge/getServer)
+        (.getPlayer ^UUID uuid))))
